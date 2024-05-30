a3d556c940da6235def012022d6ffb48311aed9f
raaasin <nisarvskp@gmail.com>
2024-05-30 16:34:07 +0530

    streaming works, but chat history not working

--- app.py
+++ app.py
@@ -3,25 +3,35 @@ import requests
 import json
 
 
+global chat_history
+chat_history = []
+
 # Function to stream responses from the Mistral model
-def stream_mistral_response(prompt):
-    url = "http://localhost:11434/api/generate"
-    payload = {"model": "mistral", "prompt": prompt}
+def stream_mistral_response():
+    url = "http://localhost:11434/api/chat"
+    payload = {"model": "mistral", "messages": chat_history}
     headers = {"Content-Type": "application/json"}
 
     response = requests.post(url, json=payload, headers=headers, stream=True)
 
     if response.status_code == 200:
-        result = ""
+        content = ""
+        total = " "
         for line in response.iter_lines():
             if line:
                 line_data = json.loads(line.decode('utf-8'))
-                result += line_data.get("response", "")
-        return result
+                content = line_data.get("message", {}).get("content", "")
+                total+=content
+                yield content
+        chat_history.append({
+            "role": "assistant",
+            "content": total
+        })
+
     else:
         st.error("Error: " + str(response.status_code))
 
-st.title("Mistral Model Interaction")
+st.title("Mistral")
 if "messages" not in st.session_state:
     st.session_state["messages"] = [{"role": "assistant", "content": "Hi I am your Llm web agent connected to internet. AMA! 😊"}]
 
@@ -31,7 +41,11 @@ for msg in st.session_state.messages:
 if prompt := st.chat_input():
     st.session_state.messages.append({"role": "user", "content": prompt})
     st.chat_message("user").write(prompt)
-    msg= stream_mistral_response(prompt)
-    st.session_state.messages.append({"role": "assistant", "content": msg})
-    st.chat_message("assistant").write(msg)
-    
+    chat_history.append({
+      "role": "user",
+      "content": prompt
+    })
+    print(chat_history)
+    with st.chat_message("assistant"):
+        response = st.write_stream(stream_mistral_response())
+    st.session_state.messages.append({"role": "assistant", "content": response})
